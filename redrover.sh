a20e0e236bc5c25aad87f1718922cff79423b013
Matthew <matthewbodaly@gmail.com>
2013-04-04 15:02:28 -0700

    Update redrover.sh
    Added support to search for 'jamf'

--- redrover.sh
+++ redrover.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # this gathers the history file and puts it somewhere common for future work
 # this section will also be broken off into a separate script so I can have the future service call the script as well as self update
-# Tasks to run:
-#  - MD5 on both sides to compare / download a new version
-#  - harden script a little bit more
 stat -f '%Su' /dev/console > /Library/Application\ Support/caspersupport/user
 USER=$(stat -f '%Su' /dev/console)
 cp -f /Users/$USER/.bash_history /Library/Application\ Support/caspersupport/history/$USER
@@ -11,3 +8,4 @@ chmod 777 /Library/Application\ Support/caspersupport/history/$USER
 # this section reads the file and creates a new file with requested information
 grep -F "sudo" /Library/Application\ Support/caspersupport/history/$USER >> /Library/Application\ Support/caspersupport/history/sudo
 grep -F "ssh" /Library/Application\ Support/caspersupport/history/$USER >> /Library/Application\ Support/caspersupport/history/ssh
+grep -F "jamf" /Library/Application\ Support/caspersupport/history/$USER >> /Library/Application\ Support/caspersupport/history/jamf
