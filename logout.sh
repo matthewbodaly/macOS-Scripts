dcc3642504df2900c2a877534cfedf7ee5a7dc3f
Matthew Bodaly <matthewbodaly@gmail.com>
2015-04-23 15:13:05 -0700

    check spelling

--- logout.sh
+++ logout.sh
@@ -5,9 +5,10 @@ systemsetup -setcomputersleep Never
 #http://hints.macworld.com/article.php?story=20040330161158532
 echo $((`ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/ !{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000))
 IDLE="$((`ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/ !{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000))"
-if [ "$IDLE" -ge "60" ] ;
+if [ "$IDLE" -ge "60
+  " ] ;
   then
   osascript -e 'tell application "System Events" to log out'
   pause 90
-  killalll loginwindow
+  killall loginwindow
 fi
