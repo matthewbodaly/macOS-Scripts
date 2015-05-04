3fa4f221ae775d8cdd940b0c0418920f917a4948
Matthew Bodaly <matthewbodaly@gmail.com>
2015-05-04 14:59:52 -0700

    Spelling

--- logout.sh
+++ logout.sh
@@ -6,18 +6,18 @@
 #https://jamfnation.jamfsoftware.com/discussion.html?id=11807
 #Make the directory for the flat file
 #I could add an if/then in here
-mkdir /Library/jamf/
+#mkdir /Library/jamf/
 #This gets the IDLE time for variable
 IDLE=$((`ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/ !{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000))
 #Writes the variable to flatfile
 #Next version to delete this file at the beginning of the script so it doesn't grow over time.
 #I made it this way to test... there should be two lines first one is idle when the script ran
 #Second should be super close when the if statement starts
-echo $IDLE >> /Library/jamf/flatfile
+#echo $IDLE >> /Library/jamf/flatfile
 #This is in seconds
 if [ "$IDLE" -ge "3600" ] ;
   then
-  echo $IDLE >> /Library/jamf/flatfile
+#  echo $IDLE >> /Library/jamf/flatfile
   osascript -e 'tell application "System Events" to log out'
   pause 90
   killall loginwindow
