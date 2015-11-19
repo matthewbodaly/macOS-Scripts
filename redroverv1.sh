6236e94f4bfb960b0236ddb7b698140f7f824e82
Matthew Bodaly <matthewbodaly@gmail.com>
2015-11-19 11:49:40 -0800

    updated getting usernam

--- redroverv1.sh
+++ redroverv1.sh
@@ -2,7 +2,7 @@
 # this gathers the history file and puts it somewhere common for future work
 # this section will also be broken off into a separate script so I can have the future service call the script as well as self update
 stat -f '%Su' /dev/console > /Library/Application\ Support/caspersupport/user
-USER=$(stat -f '%Su' /dev/console)
+USER='python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'
 cp -f /Users/$USER/.bash_history /Library/Application\ Support/caspersupport/history/$USER
 chmod 777 /Library/Application\ Support/caspersupport/history/$USER
 # this section reads the file and creates a new file with requested information
