98a3eab911afb2ed334ae19d2d10a31533612530
Matthew Bodaly <matthewbodaly@gmail.com>
2015-11-24 15:52:16 -0800

    updated this script

--- RenameComputerv3.sh
+++ RenameComputerv3.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-###
+################################## DOCUMENTATION ###################################
 #
 #            Name:  renamecomputer.sh
-#     Description:
-#          Author:
-#         Created:
-#   Last Modified:
+#     Description:  Renames computer to something that you want instead of say... the serial number.
+#          Author: Matthew Bodaly https://github.com/matthewbodaly
+#                   Serious credits to Elliot Jordan <elliot@lindegroup.com> as you might recognize the template
+#         Created: 24 November 2015
+#   Last Modified: 24 November 2015
 #         Version:  1
 #
-###
-
-
 ################################## VARIABLES ###################################
 
 COMPANY_NAME="Concur"
@@ -35,15 +33,11 @@ Click the Next button below, then enter the computer name."
 # Path to jamfHelper.
 jamfHelper="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
 
-
-################################################################################
 ######################### DO NOT EDIT BELOW THIS LINE ##########################
-################################################################################
-
 
 ######################## VALIDATION AND ERROR CHECKING #########################
 
-# Make sure the custom logo has been received successfully
+# Make sure the custom logo has been received successfully if not, download it.
 if [[ ! -f "/${LOGO_ICNS//://}" ]]; then
     echo "[ERROR] Custom icon not present: /${LOGO_ICNS//://}"
     sudo jamf policy -trigger logos
@@ -60,27 +54,28 @@ dsconfigad -force -remove -u user -p babytownfrolics
 
 # Display a branded prompt explaining the prompt.
 echo "Alerting user ${userName} about incoming prompt..."
-
 "$jamfHelper" -windowType hud -windowPosition ur -lockHUD -icon "$LOGO_PNG" -heading "$PROMPT_HEADING" -description "$PROMPT_MESSAGE" -button1 "Next" -defaultButton 1 -startlaunchd
 
 # Get the computername via a prompt
 echo "Prompting ${userName} for the computer name..."
-ComputerName="$(/usr/bin/osascript -e 'tell application "System Events" to display dialog "Please enter the computer name:" default answer "" with title "Computer Name" with text buttons {"OK"} default button 1 with hidden answer with icon file "'"${LOGO_ICNS//\"/\\\"}"'"' -e 'text returned of result')"
+ComputerName="$(/usr/bin/osascript -e 'tell application "System Events" to display dialog "Please enter the computer name:" default answer "" with title "Computer Name" with text buttons {"OK"} default button 1 with icon file "'"${LOGO_ICNS//\"/\\\"}"'"' -e 'text returned of result')"
 
+# Write the variable to a text file also. This can be called up elsewhere.
 echo "Writing Variable"
 echo $ComputerName > /Library/Application Support/Concur/assettag.txt
 
 echo "Setting Variables"
 # Change the HostName to the variable
-scutil --set HostName $ComputerName
+# scutil --set HostName $ComputerName
 # Change the localHostName to the variable
-scutil --set LocalHostName $ComputerName
+# scutil --set LocalHostName $ComputerName
 # Change the ComputerName to the variable
-scutil --set ComputerName $ComputerName
+# scutil --set ComputerName $ComputerName
 # change the Bonjour name
-systemsetup -setlocalsubnetname $ComputerName
+# systemsetup -setlocalsubnetname $ComputerName
 # Write the variable to the end of /etc/hostconfig. This uses the FQDN. If you have a FQDN... you should change this.
 # echo HOSTNAME=$ComputerName.concur.concurtech.org >> /etc/hostconfig
+sudo jamf setComputerName -name $ComputerName
 
 # Run JAMF recon
 # If this runs as part of the initial setup... could be a moot point. Keeping this in here for later.
