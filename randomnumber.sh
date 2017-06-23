ffa3961afaad7086450c09395142d9e5f7ff11a6
Matthew <matthewbodaly@users.noreply.github.com>
2017-06-22 19:59:23 -0700

    1.1

--- randomnumber.sh
+++ randomnumber.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-# Generate a random number. This is used best when you want break up computers into groups for testing. 
+# Generate a random number. This is used best when you want break up computers into groups for testing.
 # Use with a reporting app to read the number and sort your computers
 # For an example of that, refer to:
 # https://gist.github.com/matthewbodaly/7a6cfabb74d896ca609bca9f62226463
 # v 1.0
 # Matthew Bodaly - March 2017
-mkdir /Library/Application\ Support/XXX/
-sudo echo $((1 + RANDOM % 3)) > /Library/Application\ Support/XXX/digit.txt
+# v 1.1
+# Matthew Bodaly - June 2017
+# Use case: All this script does is generate a random number between 1 and 10. From this point, an extension attribute (if you have Jamf Pro) reads the digit
+# Once you have a random number, you can shard the fleet.
+# For instance, a smart group made of random number 1 would have about 10% of the total fleet.
+if [! -d "/Library/Application\ Support/XXX/" ]
+then
+    echo "Directory does not exist."
+    mkdir /Library/Application\ Support/XXX/
+else
+    echo "Directory exists."
+fi
+echo $((1 + RANDOM % 10)) > /Library/Application\ Support/XXX/digit.txt
