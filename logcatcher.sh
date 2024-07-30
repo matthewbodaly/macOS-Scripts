862b207cb179276db9a9a64aa592ff568044a10d
Matthew <matthewbodaly@users.noreply.github.com>
2024-07-30 09:06:33 -0700

    Update logcatcher.sh
    Updated current logged in user method to get usernames

--- logcatcher.sh
+++ logcatcher.sh
@@ -3,10 +3,11 @@
 # v1.1 / Matthew Bodaly /  20 October 2016
 # v1.5 / Matthew Bodaly / DoorDash / 10 October 2017
 # Updated the method to get the current user
+#v1.6 / Matthew Bodaly / Outreach.io / 30 July 2024
 #
 # This puts log files on the current users desktop so they can attach it to tickets
 # Variables
-currentUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
 dateStamp=$(date +%Y\.%m\.%d-%H%M)
 logZipName="$dateStamp-logs.zip"
 logOutputDirectory="/Users/$currentUser/Desktop/"
